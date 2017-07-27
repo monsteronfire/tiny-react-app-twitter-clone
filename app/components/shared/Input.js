6ae09019af8e02f65398fe6f2db7e67102e5b607
Shaii Ong <monsteronfire@gmail.com>
2017-07-27 10:05:46 +0800

    Add controller input component

---
+++ app/components/shared/Input.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class Input extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      name: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <input
+       type='text'
+       value={this.state.name}
+       onChange={this.handleChange}
+      />
+    )
+  }
+}
+
+export default Input;
