b18b9a9dcc2a20a65648a79a0597ff3974f96fdc
Shaii Ong <monsteronfire@gmail.com>
2017-07-29 23:03:50 +0800

    Add modal component

---
+++ app/components/shared/Modal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Modal extends React.Component {
+  render() {
+    return (
+      <div className='modal-wrapper'>
+        modal
+      </div>
+    )
+  }
+}
+
+export default Modal;
