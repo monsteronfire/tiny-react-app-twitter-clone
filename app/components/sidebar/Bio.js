07caabedb4417bc4125ef54800b80869d40d143a
Shaii Ong <monsteronfire@gmail.com>
2017-08-02 08:07:04 +0530

    Start styling bio card

--- app/components/sidebar/Bio.js
+++ app/components/sidebar/Bio.js
@@ -4,7 +4,9 @@ class Bio extends React.Component {
   render() {
     return (
       <div className='bio-wrapper'>
-        bio
+        <h2>{this.props.profile.name}</h2>
+        <h4>{this.props.profile.handle}</h4>
+        <p>{this.props.profile.bio}</p>
       </div>
     )
   }
--- app/components/sidebar/Sidebar.js
+++ app/components/sidebar/Sidebar.js
@@ -4,11 +4,17 @@ import TweetActivity from './TweetActivity';
 import FollowSuggestions from './FollowSuggestions';
 import TrendList from './TrendList';
 
+const profile = {
+  name: 'Belle Kitty',
+  handle: '@bellekitty',
+  bio: 'I didn\'t choose the kitty life, the kitty life chose me!'
+};
+
 class Sidebar extends React.Component {
   render() {
     return (
       <aside className='sidebar-wrapper'>
-        <Bio/>
+        <Bio profile={profile} />
         <TweetActivity/>
         <FollowSuggestions/>
         <TrendList/>
