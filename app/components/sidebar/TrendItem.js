79f70e7392e0579fbaf7e10a63e287c40897a846
Shaii Ong <monsteronfire@gmail.com>
2017-08-09 23:39:01 +0800

    Add structure for TrendItem component

---
+++ app/components/sidebar/TrendItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TrendItem extends React.Component {
+  render() {
+    return (
+      <div className='trend-item-wrapper'>
+        trenditem
+      </div>
+    )
+  }
+}
+
+export default TrendItem;
